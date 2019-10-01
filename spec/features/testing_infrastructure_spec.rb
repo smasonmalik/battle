78aea4b6afec9c35d38cf354f2bfeaef73624f15
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-10-01 14:59:10 +0100

    Adds player 2 hit points

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -6,5 +6,14 @@ feature 'Enter names' do
     click_button 'Submit'
     expect(page).to have_content 'James vs. Sid'
   end
+end
 
+feature 'Viewing Hitpoints' do
+  scenario 'player 2 starts with 100HP' do
+    visit('/')
+    fill_in :player_1_name, with: 'James'
+    fill_in :player_2_name, with: 'Sid'
+    click_button 'Submit'
+    expect(page).to have_content 'Sid: 100HP'
+  end
 end
